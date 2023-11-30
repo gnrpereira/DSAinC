1615bbe7c8a68940e7441ceec7d6eef59180e3ee
Gabriel Pereira <gpereiranrs@outlook.com>
2023-11-30 11:52:56 -0300

    declarar array unidimensional

---
+++ declArray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Declarar um array unidemensional
+void main(){
+
+    int num1[5];
+    char vogais[] = {'a','e','i','o','u'};
+    float notas[3] = {8.5, 6.7, 9.2};
+
+    return 0;
+}
