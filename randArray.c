e07ae396dcdafe839a2e490041bcbdab0c3307d6
Gabriel Pereira <gpereiranrs@outlook.com>
2023-11-30 18:21:27 -0300

    gerar num aleatorio

---
+++ randArray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Gerar números aleatórios para preencher o array
+void main(){
+
+    int i;
+    int num1[10];
+
+    srand(time(NULL));
+
+    for(i = 0; i < 10; i++)
+        num1[i] = rand();
+
+    printf("\n\n");
+    for(i = 0; i < 10; i++)
+        printf("%d ", num1[i]);
+    printf("\n\n");
+
+    return 0;
+}
