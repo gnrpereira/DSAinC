349688016ae4827cb93c0cb76d5dda17876d0f37
Gabriel Pereira <gpereiranrs@outlook.com>
2023-12-01 14:01:33 -0300

    imprimir matriz

--- imprMatriz.c
+++ imprMatriz.c
@@ -3,20 +3,16 @@
 // Criar uma matriz
 int main(){
 
-    int mat1[3][3] = {1,2,3,4,5,6,7,8,9}
-    /*      3 x 3 = 9 elementos
-                col0 col1 col2
-        linha0   1     2    3
-        linha1   4     5    6
-        linha2   7     8    9
-    */
+    int i, j;
+    int mat1[3][3] = {1,2,3,4,5,6,7,8,9};
+    char mat5[2][2] = {'a','b','c','d'};
 
-    int mat2[][3] = {1,2,3,4,5,6};
-    int mat3[3][3] = {0};
-
-    float mat4[4][5];
-    char mat5[10][10];
-
+    for(i = 0; i < 3; i++){
+        printf("\n\t");
+        for(j = 0; j < 3; j++){
+            printf("%d ", mat1[i][j]);
+        }
+    }
 
     return 0;
 }
