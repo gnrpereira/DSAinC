961d89a470cb9e80d57be16660498f4407e2c074
Gabriel Pereira <gpereiranrs@outlook.com>
2023-12-01 14:28:46 -0300

    ler matriz

---
+++ lerMatriz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Criar uma matriz
+int main(){
+
+    int i, j;
+    int mat1[3][3] = {1,2,3,4,5,6,7,8,9};
+    char mat5[2][2] = {'a','b','c','d'};
+
+    for(i = 0; i < 3; i++){
+
+        for(j = 0; j < 3; j++){
+           printf("Linha [%d]: \nColuna [%d]: ", i, j);
+           scanf("%d", &mat1[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++){
+        printf("\n\t");
+        for(j = 0; j < 3; j++){
+            printf("%d ", mat1[i][j]);
+        }
+    }
+
+    return 0;
+}
