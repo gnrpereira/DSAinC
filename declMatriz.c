be9da10b43b2a5f7a161dd7a447e49874b4a4c24
Gabriel Pereira <gpereiranrs@outlook.com>
2023-12-01 13:47:38 -0300

    declarar matriz

--- declMatriz.c
+++ declMatriz.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-//
+// Criar uma matriz
 int main(){
 
+    int mat1[3][3] = {1,2,3,4,5,6,7,8,9}
+    /*      3 x 3 = 9 elementos
+                col0 col1 col2
+        linha0   1     2    3
+        linha1   4     5    6
+        linha2   7     8    9
+    */
+
+    int mat2[][3] = {1,2,3,4,5,6};
+    int mat3[3][3] = {0};
+
+    float mat4[4][5];
+    char mat5[10][10];
 
 
     return 0;
